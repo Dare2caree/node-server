5b6d2b584954ce851bf7a9845534395dd1014a52
asim sheikh <asmsheikh123@gmail.com>
2022-02-25 21:45:15 +0530

    added git

---
+++ index.js
@@ -0,0 +1,34 @@
+http=require('http');
+fs=require('fs');
+url=require('url');
+
+
+http.createServer((req,res)=>{
+  
+    let q=url.parse(req.url,true);
+    
+    if(req.url!='/favicon.ico'){
+        console.log(q.pathname);
+     if(q.pathname=="/"){
+         
+        let index=fs.readFileSync('index.html');
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(index);
+     }
+     else if(q.pathname=="/profile"){
+        res.writeHead(200,{'content-type':'text/html'});
+
+         res.write('welcome asim');
+     }
+
+    else{
+        let filename=q.pathname;
+         res.writeHead(200,{"content-type":"text/html"})
+         res.write(q.pathname+' not found')
+    }
+}
+   
+}).listen(8080);
+
+console.log('server code has no error')
+
